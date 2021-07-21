8b37b486247f32e2c1e5e5e969eab9308fc464d2
Willdeakin <willdeakin@outlook.com>
2021-07-21 19:06:49 +0000

    add 404 and 500 error pages

--- app.py
+++ app.py
@@ -171,6 +171,16 @@ def delete(recipe_id):
     return redirect(url_for("recipes"))
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'),404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'),500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
